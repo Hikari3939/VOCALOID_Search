d986aba12f07e59cae2f469c6ed08aa34c4abc79
Hikari3939 <fatails_hikari@yeah.net>
2024-10-23 14:21:14 +0800

    正则表达式爬取详情

--- Spider/VOCALOID_Spider.py
+++ Spider/VOCALOID_Spider.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import logging
 import httpx
 import json
@@ -15,9 +16,16 @@ logging.basicConfig(
     ]
 )
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+                    AppleWebKit/537.36 (KHTML, like Gecko) \
+                    Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+}
 
 
-def scrape_page(url, headers):
+
+def scrape_page(url):
     logging.info('scraping %s...', url)
     
     try:
@@ -31,15 +39,25 @@ def scrape_page(url, headers):
 
 
 
+def scrape_and_save_details(url):
+    dir = 'details'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    html = scrape_page(url)
+    get_and_save_details(html, dir, '')
+
+
+
 def get_urls(html):
     urls = []
 
     raw_results = re.findall('famed-song-2.*?href="(.*?)"', html, re.S)
     for result in raw_results:
         url = 'https://mzh.moegirl.org.cn' + result
-        logging.info('get detail url %s', url)
         urls.append(url)
 
+    logging.info('get detail url %s', len(urls))
     return urls
 
 
@@ -90,7 +108,9 @@ def save_basic_information(results):
 
 
 
-def get_and_save_details(html, dir):
+def get_and_save_details(html, dir, num):
+    logging.info('scrape %s...', num)
+    
     #处理标题
     title = re.search('.*?<body class="mediawiki.*?page-(.*?) rootpage', html, re.S)
     if title:
@@ -107,42 +127,64 @@ def get_and_save_details(html, dir):
             info = info.replace(info_removed, '', 1)
         #补全链接
         info = re.sub('href="/', 'href="https://mzh.moegirl.org.cn/', info)
+        logging.info('success get info')
 
     #处理歌词
-    lyrics = re.search('(<div class="Lyrics Lyrics-.*?<div style="clear:both">.*?</div>)', html, re.S)
+    lyrics = re.search('歌词</span>\n*</h[23]>.*?(<div class="Lyrics Lyrics-.*?<div style="clear:both">\n*</div>\n*</div>)', html, re.S)
     if not lyrics:
-        lyrics = re.search('(<div class="poem">.*?</div>)', html, re.S)
+        lyrics = re.search('歌词</span>\n*</h[23]>.*?(<div class="poem">.*?</p>\n*</div>)', html, re.S)
+        if not lyrics:
+            lyrics = re.search('歌词</span>\n*</h[23]>(.*?)<h[23]>', html, re.S)
+            if not lyrics:
+                lyrics = re.search('歌词</span>\n*</h[23]>(.*?)<div style="clear: both;', html, re.S)
+    if title == '七色的黑塔利亚动画':#特殊1
+        lyrics = re.search('歌词</span>\n*</h[23]>(.*?)<h[23]>', html, re.S)
+    if lyrics:
+        #补全链接
+        lyrics = re.sub('href="/', 'href="https://mzh.moegirl.org.cn/', lyrics.group(1))
+        logging.info('success get lyrics')
+    if title == '甩葱歌':#特殊2
+        lyrics = re.search('(<div style="color:#39C5BB">.*?</div>\n*</div>).*?</h3>.*?(<div class="poem">.*?</div>)', html, re.S)
+        lyrics = lyrics.group(1)+'\n'+lyrics.group(2)
+
 
     #存入文件
     if title:
-        path = '%s/%s.html' % (dir, title)
+        path = '%s/%s_%s.html' % (dir, num, title)
         with open(path,'w',encoding='utf-8') as file:
             if info:
                 file.write(info+'\n\n\n')
             if lyrics:
-                file.write(lyrics.group(1))
+                file.write(lyrics)
 
 
 
-url = 'https://mzh.moegirl.org.cn/VOCALOID%E4%BC%A0%E8%AF%B4%E6%9B%B2'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-                    AppleWebKit/537.36 (KHTML, like Gecko) \
-                    Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-}
+if __name__ =='__main__':
+    url = 'https://mzh.moegirl.org.cn/VOCALOID%E4%BC%A0%E8%AF%B4%E6%9B%B2'
 
-#爬取基本信息
-html = scrape_page(url, headers)
-results = get_basic_information(html)
+    #爬取基本信息
+    html = scrape_page(url)
+    results = get_basic_information(html)
 
-save_basic_information(results)
+    save_basic_information(results)
 
-#爬取详细信息
-dir = 'details'
-if not os.path.exists(dir):
-    os.makedirs(dir)
-urls = get_urls(html)
-for url in urls:
-    html = scrape_page(url, headers)
-    get_and_save_details(html, dir)
+    #爬取详细信息
+    
+    #多进程方式(被ban了)
+    # pool = multiprocessing.Pool()
+    # urls = get_urls(html)
+    # pool.map(scrape_and_save_details, urls)
+    # pool.close()
+    # pool.join()
+    
+    #单进程方式
+    urls = get_urls(html)
+    dir = 'details'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    num = 1
+    for url in urls:
+        if num >= 1 and num <= 1000:
+            html = scrape_page(url)
+            get_and_save_details(html, dir, num)
+        num += 1
